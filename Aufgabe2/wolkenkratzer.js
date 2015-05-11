ba5b6107c7ab7d95b6ddb7d11363f1a0d34bbf60
pmirau <p.mirau95@web.de>
2015-05-11 10:22:19 +0200

    Update wolkenkratzer.js

--- Aufgabe2/wolkenkratzer.js
+++ Aufgabe2/wolkenkratzer.js
@@ -2,7 +2,7 @@ var chalk = require('chalk');
 var fs = require('fs');
 fs.readFile(__dirname + "/wolkenkratzer.json", 'utf8', function (err, data){
     if (err) {
-        console.log('Error: ' + err); return;
+        console.log(err); return;
     }
 var dez = JSON.parse(data.toString());
 
