9e63d4e81eb870b19065be8ec9ad512179b06aa3
pmirau <p.mirau95@web.de>
2015-05-11 10:12:52 +0200

    Update wolkenkratzer.js

--- Aufgabe1/wolkenkratzer.js
+++ Aufgabe1/wolkenkratzer.js
@@ -1,10 +1,13 @@
 var fs = require('fs');
 fs.readFile(__dirname + "/wolkenkratzer.json", 'utf8', function (err, data){
     if (err) {
-        console.log('Error: ' + err); return;
+        console.log(err); return;
     }
-    var str = JSON.parse(data.toString());
-    str.wolkenkratzer.forEach(function(entry){
-        console.log('\nName:' + entry.name + '\nStadt:' + entry.stadt + '\nHoehe:' + entry.hoehe + 'm' + '\n\n--------------------'); 
+    var dez = JSON.parse(data.toString());
+    dez.wolkenkratzer.forEach(function(entry){
+        console.log('\nName:' + entry.name);
+        console.log('\nStadt:' + entry.stadt);
+        console.log('\nHoehe:' + entry.hoehe);
+        console.log('\n\n--------------------');
     });
 });
